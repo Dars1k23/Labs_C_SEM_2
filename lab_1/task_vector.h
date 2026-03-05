8615c826a569306062aa6e71717a0867b2a72005
Dars1k23 <sanchous-2017@mail.ru>
2026-03-05 20:24:14 +0300

    Create task_vector.h

---
+++ lab_1/task_vector.h
@@ -0,0 +1,9 @@
+#ifndef TASK_VECTOR_H
+#define TASK_VECTOR_H
+
+Vector *init_vector(Matrix* matr);
+int search_max_c(int *data, int len);
+void print_vector(Vector* vec);
+void free_vector(Vector* vec);
+
+#endif
